62cde5499f0c7cbfc91b8f2f56a73db44aca735c
Matine temory <temorymatine@gmail.com>
2020-05-23 22:58:32 +0000

    Automatically backed up by Learn

--- lib/array_count.rb
+++ lib/array_count.rb
@@ -1,8 +1,15 @@
 def count_strings(array)
 
 array2 = []
-count2 = 0
-if array
+count2 = 0 
+while array.count > count2 
+if array[count].is_a?(Integer)
+  count2+=1 
+else
+  array2 << array[count]
+  count2+=1
+end
+array2.count 
   
 end
 
