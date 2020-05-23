02cc8f30b6596d9b485e5f127e4da24971627713
Matine temory <temorymatine@gmail.com>
2020-05-23 23:05:54 +0000

    Automatically backed up by Learn

--- lib/array_count.rb
+++ lib/array_count.rb
@@ -2,16 +2,20 @@ def count_strings(array)
 
 array2 = []
 count2 = 0 
-  while array.count > count2 
-  if array[count2].is_a?(String)
-  array2 << array[count2]
-  count2+=1 
-  else
-  count2+=1
-  end
 
-end
+  while array.count > count2 
+  
+         if array[count2].is_a?(String)
+              array2 << array[count2]
+              count2+=1 
+          else
+             count2+=1
+         end
+
+    end
+
 array2.count
+
 end
 
 def count_empty_strings(array)
