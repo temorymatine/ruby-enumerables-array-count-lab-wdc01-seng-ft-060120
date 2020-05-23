8f3f54708f252183a7fa42acb3dc5ac7fad6ec08
Matine temory <temorymatine@gmail.com>
2020-05-23 23:07:12 +0000

    Automatically backed up by Learn

--- lib/array_count.rb
+++ lib/array_count.rb
@@ -21,5 +21,25 @@ array2.count
 end
 
 def count_empty_strings(array)
-  a
+  
+  
+  array2 = []
+count2 = 0 
+
+  while array.count > count2 
+  
+         if array[count2].any?("")
+           
+              array2 << array[count2]
+              count2+=1 
+              
+          else
+             count2+=1
+         end
+
+    end
+  
+  
+  array2.count
+  
 end
