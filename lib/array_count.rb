dac32ba562c9243816cd8c108de874e78254c89c
Matine temory <temorymatine@gmail.com>
2020-05-23 23:10:50 +0000

    Done.

--- lib/array_count.rb
+++ lib/array_count.rb
@@ -1,7 +1,45 @@
 def count_strings(array)
-  # Return the total number of strings in the provided array using the count enumerable
+
+array2 = []
+count2 = 0 
+
+  while array.count > count2 
+  
+         if array[count2].is_a?(String)
+           
+              array2 << array[count2]
+              count2+=1 
+              
+          else
+             count2+=1
+         end
+
+    end
+
+array2.count
+
 end
 
 def count_empty_strings(array)
-  # Return the total number of EMPTY strings in the provided array using the count enumerable
+  
+  
+  array2 = []
+count2 = 0 
+
+  while array.count > count2 
+  
+         if array[count2] == ""
+           
+              array2 << array[count2]
+              count2+=1 
+              
+          else
+             count2+=1
+         end
+
+    end
+  
+  
+  array2.count
+  
 end
